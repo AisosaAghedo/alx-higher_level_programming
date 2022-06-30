d38dbfcb750111ea5f0ac8c33173bcfd30306567
Aisosa <allysonaghedo@gmail.com>
2022-06-30 14:40:28 +0100

    prints the result of the addition of all arguments infinitely

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,14 +4,11 @@ if __name__ == "__main__":
     from sys import argv
     length = len(argv) - 1
     i = 1
-    sum_total = 0 
+    sum_total = 0
     if length == 0:
         print(0)
     else:
         while i <= length:
-            sum_total = sum_total + int (argv[i])
+            sum_total = sum_total + int(argv[i])
             i = i + 1
         print(sum_total)
-
-
-
