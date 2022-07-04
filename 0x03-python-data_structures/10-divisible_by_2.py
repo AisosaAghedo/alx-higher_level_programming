ed358eeda5089cf1fd7b15dc6d7652abbc0e54f2
Aisosa <allysonaghedo@gmail.com>
2022-07-04 20:31:08 +0100

    python data structures: list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -7,12 +7,3 @@ def divisible_by_2(my_list=[]):
         else:
             new_list.append(False)
     return(new_list)
-
-
-my_list = [0, 1, 2, 3, 4, 5, 6]
-new_list = divisible_by_2(my_list)
-
-i = 0
-while i < len(new_list):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if new_list[i] else "is not"))
-    i += 1
