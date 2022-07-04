4bc50ff1ddd888e18fb5333f33c2ce33b696af8b
Aisosa <allysonaghedo@gmail.com>
2022-07-04 14:11:14 +0100

    python data structures: list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    maxi = 0
+    i =  0
+    length = len(my_list)
+    if length == 0:
+        return(None)
+    else:
+        while i < length:
+            maxi = maxi[i]
+            i = i + 1
+    return(maxi)
