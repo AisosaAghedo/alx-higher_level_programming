4a0c41474408130f6013be8c7eba24aefb07051b
Aisosa <allysonaghedo@gmail.com>
2022-07-06 18:31:26 +0100

    delete from  a dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key != None:
+    if key = None:
+        return(a_dictionary)
+    else:
         del a_dictionary[key]
-    return(a_dictionary)
+        return(a_dictionary)
