e21c0bb1c59cf540618c2dd0d4dbf066d3077e21
Aisosa <allysonaghedo@gmail.com>
2022-07-06 21:33:30 +0100

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
+    if not a_dictionary:
         return(None)
     return(max(zip(a_dictionary.values(), a_dictionary.keys()))[1])
-
-
