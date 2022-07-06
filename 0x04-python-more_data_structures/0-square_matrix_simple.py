048a1d94d31fe97e6c2061b996ba814e9f59eb1a
Aisosa <allysonaghedo@gmail.com>
2022-07-06 15:01:27 +0100

    square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    length = len(matrix)
+    new_list = [[] * i for i in range(length)]
+    
+    i = 0
+    while i < length:
+        new_list[i] = list(map((lambda(x: x ** 2),matrix[i] ))
+        i = i + 1
+    return(new_list)
+
