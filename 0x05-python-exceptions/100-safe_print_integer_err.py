3fa883ed50806a83503f5e4456d82cd41041529f
Aisosa <allysonaghedo@gmail.com>
2022-07-18 22:06:17 +0100

    optional task

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -9,18 +9,3 @@ def safe_print_integer_err(value):
     except (ValueError, TypeError) as err:
         sys.stderr.write("Exception:{}\n".format(err))
         return False
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
