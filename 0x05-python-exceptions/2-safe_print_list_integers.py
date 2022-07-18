753181863e04b7389713658ea3dba7dca4567f9c
Aisosa <allysonaghedo@gmail.com>
2022-07-18 16:15:25 +0100

    safe print jus integer and ignore other data types

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+     li = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            li = li + 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return(li)
