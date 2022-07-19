787c3bfe15a74130f6523ba18b8ddf9e3207c4ae
Aisosa <allysonaghedo@gmail.com>
2022-07-19 17:36:29 +0100

    constructor of the class 'Square' with its size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" defines class Square """
+
+class Square:
+    """ defines the class square"""
+
+    def __init(self, size=0):
+        """ constructor of class 'Square' with the size """
+
+        if type(size) is not int:
+            raise (TypeError("size must be an integer"))
+        elif size < 0:
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
