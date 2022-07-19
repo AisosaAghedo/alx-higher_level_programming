4c29db38de608eefbabdb3a21f8597d3a1424827
Aisosa <allysonaghedo@gmail.com>
2022-07-19 22:46:37 +0100

    python getters and setters

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,15 +8,18 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """initialisation of a Square with the size and position"""
+
         self.size = size
         self.position = position
 
     def area(self):
         """retuns the area of the Square"""
+
         return (self.__size ** 2)
 
     def my_print(self):
         """  that prints in stdout the square with the character # """
+
         if (self.__size == 0):
             print()
         else:
@@ -29,11 +32,13 @@ class Square:
     @property
     def size(self):
         """retrieves the private attribute size"""
+
         return (self.__size)
 
     @size.setter
     def size(self, value):
         """Sets the size private attribute"""
+
         if (type(value) is not int):
             raise (TypeError("size must be an integer"))
         elif (value < 0):
@@ -44,11 +49,13 @@ class Square:
     @property
     def position(self):
         """retrieves  the Position"""
+
         return (self.__position)
 
     @position.setter
     def position(self, value):
         """Sets the position"""
+
         if (len(value) != 2) or (type(value) is not tuple) \
                 or (type(value[0]) is not int) \
                 or (type(value[1]) is not int) \
@@ -56,22 +63,3 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
-
-
-Square = __import__('6-square').Square
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
-
