246bbae130ea601c6fe401ca452d7bb92c0f41d6
Aisosa <allysonaghedo@gmail.com>
2022-07-19 16:51:52 +0100

    constructor of the class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines a class Square"""
+
+class Square:
+    """ Defines a class with a private instance attribute size """
+    
+    def __init__(self, size)
+    """ constructor of Square """
+    
+    self.__size = size
