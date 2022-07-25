7f0d1d967eb9972b087cf4b3e31ec2d668039a1b
Aisosa <allysonaghedo@gmail.com>
2022-07-25 23:31:01 +0100

    nqueens

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+nqueens backtracking program to print the coordinates of n queens
+on an nxn grid such that they are all in non-attacking positions
+"nqueen puzzle"
+"""
+
+
+from sys import argv
+
+if __name__ == "__main__":
+    new_list = []
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if argv[1].isdigit() is False:
+        print("N must be a number")
+        exit(1)
+    n = int(argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    # initialize the answer list
+    for i in range(n):
+        new_list.append([i, None])
+
+    def already_exists(y):
+        """check that a queen does not already exist in that y value"""
+        for x in range(n):
+            if y == new_list[x][1]:
+                return True
+        return False
+
+    def reject(x, y):
+        """determines whether or not to reject the solution"""
+        if (already_exists(y)):
+            return False
+        i = 0
+        while(i < x):
+            if abs(new_list[i][1] - y) == abs(i - x):
+                return False
+            i += 1
+        return True
+
+    def clear_new_list(x):
+        """clears the answers from the point of failure on"""
+        for i in range(x, n):
+            new_list[i][1] = None
+
+    def nqueens(x):
+        """recursive backtracking function to find the solution"""
+        for y in range(n):
+            clear_new_list(x)
+            if reject(x, y):
+                new_list[x][1] = y
+                if (x == n - 1):  # accepts the solution
+                    print(new_list)
+                else:
+                    nqueens(x + 1)  # moves on to next x value to continue
+
+    # start the recursive process at x = 0
+    nqueens(0)
