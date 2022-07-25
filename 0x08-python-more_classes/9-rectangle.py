3018e9f5928a4bd76b05836801a4e0f1b9a6d7e8
Aisosa <allysonaghedo@gmail.com>
2022-07-25 23:17:36 +0100

    class methods

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -84,9 +84,3 @@ class Rectangle:
     def square(cls, size=0):
         """ reurns a  new rectangle instance """
         return cls(size, size)
-
-my_square = Rectangle.square(5)
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-print(my_square)
-
-
