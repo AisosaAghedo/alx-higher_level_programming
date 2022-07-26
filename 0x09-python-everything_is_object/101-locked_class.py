bb27151f55b3a5f2fa29e41377eadf71db8dc0af
Aisosa <allysonaghedo@gmail.com>
2022-07-26 21:13:17 +0100

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+""" creation of a class  Lockedclass """
+
+
 class LockedClass:
+    """ assigning memory for only 'first_name
+    using '__slots__'"""
+    
     __slots__ = ["first_name"]
