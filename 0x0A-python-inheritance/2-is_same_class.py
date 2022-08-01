7ee3a779d2891da808d39b75af99018d0bc24781
Aisosa <allysonaghedo@gmail.com>
2022-08-01 22:02:49 +0100

    sub class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,7 @@
 
 
 def is_same_class(obj, a_class):
+    """ returns true or false """
     if type(obj) == a_class:
         return True
     else:
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,5 @@ returns True if the object is an instance of a class that inherited
 
 
 def inherits_from(obj, a_class):
+    """ returns true or false """
     return issubclass(type(obj), a_class) and type(obj) != a_class
