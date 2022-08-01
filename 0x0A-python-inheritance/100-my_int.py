99ad61e54c10184eb685048c989f5e9f8ead9b18
Aisosa <allysonaghedo@gmail.com>
2022-08-02 00:27:17 +0100

    advanced

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+class int and subclass Myint
+"""
+
+class MyInt(int):
+    """ inherits from class int """
+
+    def __eq__(self, value):
+        if (self - value == 0):
+            return False
+        else:
+
+            return True
+
+    def __ne__(self, value):
+        if self - value != 0:
+            return False
+        else:
+            return True
