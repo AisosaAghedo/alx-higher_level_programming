d25b62f9211869d2789b5718a61af0576e4f368e
Aisosa <allysonaghedo@gmail.com>
2022-08-02 00:17:18 +0100

    subclass of a subclass

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -43,14 +43,14 @@ class Rectangle(BaseGeometry):
 
 
 class Square(Rectangle):
-      """ sub class of a sub class """
+    """ sub class of a sub class """
 
-      def __init__(self, size):
+    def __init__(self, size):
         """ instantiation """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
-      def __str__(self):
+    def __str__(self):
         """ returns square descripion"""
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
