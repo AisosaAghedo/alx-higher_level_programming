428f8be43e9021fbf37500163d433513a364d2d5
Aisosa <allysonaghedo@gmail.com>
2022-08-02 21:00:16 +0100

    class student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+a class student
+"""
+
+
+class Student:
+    """ instantiation """
+    def __init__(self, first_name, last_name, age):
+        """ publicinstance attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self):
+        """ public method """
+        return self.__dict__
