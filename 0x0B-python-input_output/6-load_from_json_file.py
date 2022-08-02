b72ebd1b0fad699a11480b7dddf684627e752b87
Aisosa <allysonaghedo@gmail.com>
2022-08-02 13:53:14 +0100

    json file using load

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from json import load
+"""
+ function that creates an Object from a “JSON file”
+ """
+
+
+def load_from_json_file(filename):
+    """ creates an object """
+    with open(filename, encoding="utf-8") as f:
+        return load(f)
