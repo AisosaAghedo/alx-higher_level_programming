0b1014c21c3a77f95f546f40ce350b99941da16c
Aisosa <allysonaghedo@gmail.com>
2022-08-06 00:01:31 +0100

    first 4 tasks

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A class named base """
+
+
+class Base:
+    """ private class attribute """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ constructor """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+""" A sub class Rectangle """
+from  models.base import Base
+
+
+class Rectangle(Base):
+    """ constructor """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    """ retrieves private attribute x """
+    @property
+    def x(self):
+        return self.__x
+
+    """ sets private attribute x """
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    """ retrieves private attribute y """
+    @property
+    def y(self):
+        return self.__y
+
+    """ sets private attribute y """
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
+
+    """ retrieves private attribute width"""
+    @property
+    def width(self):
+        return self.__width
+
+    """ sets private attribute width """
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+
+    """ retrieves private attribute height """
+    @property
+    def height(self):
+        return self.__height
+
+    """ sets private attribute y """
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    def area(self):
+        """ returns area of a rectangle """
+        return self.__width * self.__height
+
+    def display(self):
+        """print to stdout"""
+        for i in range(self.__height):
+            print("#" * self.__width)
+
+    def __str__(self):
+        """ __str__ representation of the rectangle"""
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id, self.__x, self.__y, self.__width, self.__height)
