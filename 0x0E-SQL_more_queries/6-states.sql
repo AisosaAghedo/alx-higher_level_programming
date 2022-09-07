5c8072be2c6fb54799f741b59e269eaff51739b9
Aisosa <allysonaghedo@gmail.com>
2022-09-07 20:49:59 +0100

    create table in database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 --  a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT unique, AUTO_INCREMENT NOT NULL PRIMAR KEY, name VARCHAR(256) NOT NULL)
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT UNIQUE, AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
