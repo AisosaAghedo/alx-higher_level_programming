59354db8b7058a371fbccc1343fe470f6687f066
Aisosa <allysonaghedo@gmail.com>
2022-10-03 23:32:55 +0100

    more

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 let secondLargest = 0;
-const number = parseInt(process.argv.slice(2));
+const number = process.argv.slice(2);
 if (number.length > 1) {
   number.sort();
   secondLargest = number[number.length - 2];
