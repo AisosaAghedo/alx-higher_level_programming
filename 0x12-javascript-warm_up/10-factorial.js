a23c9101b2d333b49f4355793ac06c0b20523e2f
Aisosa <allysonaghedo@gmail.com>
2022-10-03 23:23:59 +0100

    more

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const process = require('process');
+const number = parseInt(process.argv[2]);
+function factorial (number) {
+  if (!number || number === 1) {
+    return (1);
+  } else {
+    return (number * factorial(number - 1));
+  }
+}
+console.log(factorial(number));
---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+let secondLargest = 0;
+let number = process.argv.slice(2);
+if (number.length > 1) {
+  number.sort();
+  secondLargest = number[number.length - 2];
+}
+console.log(secondLargest);
