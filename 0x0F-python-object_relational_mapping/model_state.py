b3a6aecbc9fe8fd8ab7567d86f32f8b35741b8a9
Aisosa <allysonaghedo@gmail.com>
2022-10-08 22:54:42 +0100

    sqlachemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,6 +8,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ class state that inherits from Base"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
