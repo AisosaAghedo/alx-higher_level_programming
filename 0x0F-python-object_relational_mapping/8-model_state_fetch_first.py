48eca68ab87be3269398de40537fd6864f2f9997
Aisosa <allysonaghedo@gmail.com>
2022-10-09 14:05:45 +0100

    sqlalchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                             argv[1], argv[2], argv[3]), pool_pre_ping=True)
     session = Session(bind=engine)
     instance = session.query(State).order_by(State.id).first()
-    if (State is None):
+    if (instance is None):
         print("Nothing")
     else:
         print("{}: {}".format(instance.id, instance.name))
