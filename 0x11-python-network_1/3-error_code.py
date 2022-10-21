395567bb00471e786c65b4cfba5fba946e0ffc50
Aisosa <allysonaghedo@gmail.com>
2022-10-21 20:30:02 +0100

    urllib

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""  Python script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)"""
+if __name__ == '__main__':
+    from sys import argv
+    from urllib.error import HTTPError
+    from urllib.request import urlopen
+    url = argv[1]
+    try:
+        with urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code)0)
+
