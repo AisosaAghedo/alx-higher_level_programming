8cabf2a00a7816c2371c1cfde60dbe850304b7c7
Aisosa <allysonaghedo@gmail.com>
2022-10-21 21:09:57 +0100

    requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a
+parameter, and finally displays the body of the response"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    url = argv[1]
+    value = {'email': argv[2]}
+    r = requests.post(url, data=value)
+    print(r.text)
