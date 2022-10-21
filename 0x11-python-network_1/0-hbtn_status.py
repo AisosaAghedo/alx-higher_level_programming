a9979a5c1c5ae698ae4041e265112617cedc438a
Aisosa <allysonaghedo@gmail.com>
2022-10-21 08:49:01 +0100

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ Python script that fetches https://alx-intranet.hbtn.io/status"""
-
-from urllib.request import urlopen
-url = 'https://alx-intranet.hbtn.io/status'
-with urlopen(url) as response:
-    script = response.read()
-    print("- type: {}".format(type(script)))
-    print("- content: {}".format(script))
-    print("- utf8 content: {}".format(script.decode('utf-8')))
-
+if __name__ == "__main__":
+    from urllib.request import urlopen
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urlopen(url) as response:
+        script = response.read()
+        print("- type: {}".format(type(script)))
+        print("- content: {}".format(script))
+        print("- utf8 content: {}".format(script.decode('utf-8')))
